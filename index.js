67527f5b553c0fd9b7af58db9d185371c84d32fe
Seth DImmer <sethdimmer1@gmail.com>
2022-12-18 15:16:35 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // "https://api.tvmaze.com/search/shows?q="
 
+const url = "https://api.tvmaze.com/search/shows?q="
 
+async function main(movie) {
+  const movies = await fetch(url + movie)
+  
+}
 
